444c4640905c45d0de0b359743509ce8d21fff20
dreamonus <dmarkin@ua.fm>
2015-02-25 17:27:44 +0200

    Add comments

--- js/tags.js
+++ js/tags.js
@@ -1,15 +1,19 @@
-'use strict';
+﻿'use strict';
+
 $(document).on('ready', function () {
     var ENTER_CODE = 13;
+
+    //main function
     function TagCloud(node) {
         this.$node = node;
-        this.tags = Array.prototype.slice.call(arguments).slice(1);
-        this.tags = getUnique(this.tags);
-		this.$node.find('input').val('');
+        this.tags = Array.prototype.slice.call(arguments).slice(1); //create start list of tags
+        this.tags = getUnique(this.tags); //create start list of unique tags
+	this.$node.find('input').val('');
         this.init();
         this.setListeners();
     }
 
+    //create DOM for tag cloud
     TagCloud.prototype.init = function () {
         this.edit = false;
         this.$node.append($('<button>').addClass('btn btn-info edit-read-btn').attr({'type': 'button'}).text('Click and edit!'))
@@ -21,22 +25,26 @@ $(document).on('ready', function () {
                     'type': 'button',
                     'disabled': true
                 }).text('Add tag!')));
+        //add start list tag elements to widget
         this.tags.forEach((function (elem) {
             this.addTag(elem);
         }).bind(this));
     };
 
+    //create handlers for widget
     TagCloud.prototype.setListeners = function () {
         var _this = this;
         this.$node.find('.edit-read-btn').on('click', function () {
-            if (_this.edit === true) {//turn on reading mode
+            if (_this.edit === true) {
+                //turn on reading mode
                 $(this).text('Click and edit!');
                 _this.$node.find('.btn-danger').hide();
                 _this.$node.find('.add-btn').attr({'disabled': true});
                 _this.$node.find('.close-button').hide();
                 _this.$node.find('input').off('keyup');
                 _this.edit = false;
-            } else {//turn on editing mode
+            } else {
+                //turn on editing mode
                 $(this).text('Click and read!');
                 _this.$node.find('.btn-danger').show();
                 _this.$node.find('.add-btn').attr({'disabled': false});
@@ -50,14 +58,17 @@ $(document).on('ready', function () {
             }
         });
 
+        //set handler for close button of element
         this.$node.find('.btn-danger').on('click', (function () {
             this.$node.find('li').remove();
-			this.tags = [];
+	    this.tags = [];
         }).bind(this));
 
+        //set handler for adding new tags to widget
         this.$node.find('.add-btn').on('click', this.addFromInputTag.bind(this));
     };
 
+    //function for adding new tags to widget from input
     TagCloud.prototype.addFromInputTag = function () {
         var inp = this.$node.find('input');
         this.input = inp.val().trim();
@@ -67,6 +78,7 @@ $(document).on('ready', function () {
         }
     };
 
+    //function for adding new tags to widget
     TagCloud.prototype.addTag = function (tag) {
         var _this = this;
         var newTag = $('<li>')
@@ -74,6 +86,8 @@ $(document).on('ready', function () {
             .append($('<span>').text(tag))
             .append($('<span class="close-button">').text(' | ×'))
             .appendTo(this.$node.find('.tag-cloud-list'));
+
+        //set handler for removing new element
         newTag.find('.close-button').on('click', function () {
             $(this).parent().remove();
             _this.tags.splice(_this.tags.indexOf(tag), 1);
@@ -88,6 +102,7 @@ $(document).on('ready', function () {
         this.$node.find('input').val('');
     };
 
+    //create unique list of elements
     function getUnique(list) {
         list = Array.prototype.slice.call(list);
         return list.filter(function (elem, i, list) {
--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
-**Widget TagList (TagCloud)**:
-* By clicking the widget is switched from the "Edit" to "View" mode. The button changes the own text depending on the mode.
+﻿**Widget TagList (TagCloud)**:
+* By clicking the widget is switched from the "Edit Mode" to "View Mode". The button changes the own text depending of the mode.
 * TagList can be initialized with predefined tags list that is passed an optional argument.
 * In the "View mode" the tags look like a list of tags.
 * In the "Edit mode" tags can be removed (next to the tags button appears removal, clicking on which the tag is removed from the list) and added (via an input field with a button). After adding the tag (if you click on the button or by pressing Enter when the focus in the input field), the input field is cleared. Add the tag cann't be possible if it added already. Spaces at the beginning and end of the line does not affect the tag (" John " and "John" is the same tag and is added to the list of "John").
 * In the "Edit mode" by pressing the delete button all tags in the tag list is cleared.
 * The page can operate with several such widgets.
 *Widget works in IE9 +.*
+
 
 **Виджет TagList (TagCloud)**:
 * По кнопке виджет переключается из режима "Редактирования" в режим "Просмотра". Кнопка меняет свой текст в зависимости от режима.
